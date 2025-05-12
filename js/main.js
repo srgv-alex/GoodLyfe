7bd1f428afb576af0bb1e4d1d3fae7fd537a6ef6
unknown <jarulcrip@gmail.com>
2025-05-12 16:00:45 +0300

    fix burger menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const HeaderSearchOpen = document.querySelector('#search-open');
+	const SearchForm = document.querySelector('#header-search-form');
 
+	HeaderSearchOpen.addEventListener('click', () => {
+		SearchForm.classList.toggle('search-form--open');
+	});
+});
 
+const burger = document.querySelector('#menu-toggle'); // это твоя кнопка
+const menuNav = document.querySelector('#header-menu'); // это меню
+
+burger?.addEventListener('click', function () {
+	burger.classList.toggle('open'); // анимация бургер-кнопки
+	menuNav?.classList.toggle('active'); // показать/скрыть меню
+});
