534dfee5990c66af3766c514d590097a256d0f11
unknown <jarulcrip@gmail.com>
2025-05-19 19:59:50 +0300

    add main slider

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 	let headerSwiper = new Swiper('.header-slider', {
 		loop: true,
-		// effect: 'fade',
+		speed: 1000,
+		preloadImages: false,
+		lazy: {
+			loadPrevNext: true,
+			loadOnTransitionStart: true,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+	let evolveSwiper = new Swiper('.evolve-slider', {
+		loop: true,
 		speed: 1000,
 		preloadImages: false,
 		lazy: {
